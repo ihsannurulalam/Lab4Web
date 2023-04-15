08028be87997caf09b3d34763347d2ff5fa41168
ihsan <ihsannurulalam276@gmail.com>
2023-04-16 02:21:00 +0700

    source code

---
+++ source/kontak.php
@@ -0,0 +1,9 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Ihsan Nurul Alam</p>
+        <p>Email : ihsannoeroel276@gmail.com</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
